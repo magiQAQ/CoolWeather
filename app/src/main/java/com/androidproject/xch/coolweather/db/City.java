ccaf3cca6c270cf5a040af3796227193ab9fb6b8
magiQAQ <39616939+magiQAQ@users.noreply.github.com>
2018-07-21 16:53:17 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/androidproject/xch/coolweather/db/City.java
@@ -0,0 +1,43 @@
+package com.androidproject.xch.coolweather.db;
+
+import org.litepal.LitePal;
+
+public class City extends LitePal {
+
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/com/androidproject/xch/coolweather/db/Province.java
@@ -0,0 +1,34 @@
+package com.androidproject.xch.coolweather.db;
+
+import org.litepal.LitePal;
+
+public class Province extends LitePal {
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
